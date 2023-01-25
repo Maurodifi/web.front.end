418beb4bbb96de52a4cce68baaea4541e3df4c9c
Mauro_Difilippo <maurodifi16@gmail.com>
2023-01-25 17:50:15 -0300

    components

--- src/components/Content/content.jsx
+++ src/components/Content/content.jsx
@@ -1,18 +1,53 @@
 import React from 'react'
-import { Text, Box } from '@chakra-ui/react'
+import { Text, SimpleGrid, Card, CardHeader, Heading, CardBody, CardFooter} from '@chakra-ui/react'
 import './content.css'
 export default function content() {
   return (
     <div className='container-content'>
-        <Text
-            bgGradient='linear(to-l, #3B3204, #3B3204)'
-            bgClip='text'
-            fontSize='6xl'
-            fontWeight='extrabold' textAlign={"center"}
-            >
-            ¿Quienes somos?
-        </Text>
-
+        <div className='container-textImg'>
+          <div className='title-img'>
+            <h3>Amamos a los perros</h3>
+            <p>Asi como cada uno ama a las personas nosotros le damos el mismo amor a los animales</p>
+            <p>Es por eso que nos embarcamos en la busqueda de perros en situacion de abandono y darle la oportunidad de darles una nueva vida</p>
+          </div>
+          <img src="https://images.pexels.com/photos/1564506/pexels-photo-1564506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Foto_perro" className='img-content'/>
+        </div>
+        <h3>Testimonios</h3>
+        <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+    <Card>
+    <CardHeader>
+      <Heading size='md'> Almendra</Heading>
+    </CardHeader>
+    <CardBody>
+      <img src="https://images.pexels.com/photos/5951991/pexels-photo-5951991.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="perra-almendra" />
+      <Text>View a summary of all your customers over the last month.</Text>
+    </CardBody>
+    <CardFooter>
+    </CardFooter>
+  </Card>
+  <Card>
+    <CardHeader>
+      <Heading size='md'> Customer dashboard</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>View a summary of all your customers over the last month.</Text>
+    </CardBody>
+    <CardFooter>
+      
+    </CardFooter>
+  </Card>
+  <Card>
+    <CardHeader>
+      <Heading size='md'> Customer dashboard</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>View a summary of all your customers over the last month.</Text>
+    </CardBody>
+    <CardFooter>
+      
+    </CardFooter>
+  </Card>
+</SimpleGrid>
     </div>
   )
 }
